f912ff67f3a257fc0039ca78d7128167ab23850f
xxai.art <xxai.art@gmail.com>
2023-08-19 08:50:51 +0800

    .

--- clippy.sh
+++ clippy.sh
@@ -14,7 +14,7 @@ git add -u && git commit -m'.' || true
 
 dasel ".workspace.members.all()" -r toml -f Cargo.toml | xargs cargo fmt -p
 
-cargo +nightly clippy --fix -Z unstable-options -- \
+cargo --all-features +nightly clippy --fix -Z unstable-options -- \
   -A clippy::uninit_assumed_init
 
 cargo fmt
