1eb26702e69baf20088b264130504df6497967cc
xxai.art <xxai.art@gmail.com>
2023-08-19 08:50:12 +0800

    .

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,14 @@ pub fn b64d(bin: impl AsRef<[u8]>) -> Result<Vec<u8>, DecodeError> {
   URL_SAFE_NO_PAD.decode(bin)
 }
 
-#[cfg(feature = "intbin")]
-mod int;
+#[cfg(feature = "u64")]
+mod u64;
 
-#[cfg(feature = "intbin")]
-pub use int::{b64_u64, u64_b64};
+#[cfg(feature = "u64")]
+pub use u64::{b64_u64, u64_b64};
+
+#[cfg(feature = "u64li")]
+mod u64li;
+
+#[cfg(feature = "u64li")]
+pub use u64li::{b64_decode_u64_li, bin_u64_li};
